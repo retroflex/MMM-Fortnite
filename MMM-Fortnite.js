1437ef039b77806dbc077a9fc3f8facbd0f72de2
retroflex <jojjepersson@yahoo.se>
2019-05-03 14:13:20 +0200

    Added code

---
+++ MMM-Fortnite.js
@@ -0,0 +1,117 @@
+/* Magic Mirror
+ * Module: MMM-Fortnite
+ *
+ * By Johan Persson, https://github.com/retroflex
+ * MIT Licensed.
+ */
+
+Module.register('MMM-Fortnite', {
+	// Default configuration.
+	defaults: {
+		showScore: true,
+		showMatchesPlayed: true,
+		showKills: true,
+		userIDs: [ '4735ce9132924caf8a5b17789b40f79c' ],  // Ninja.
+		fetchInterval: 60 * 1000  // In millisecs. Default each minute.
+	},
+
+	getStyles: function() {
+		return [ 'modules/MMM-Fortnite/MMM-Fortnite.css' ];
+	},
+
+	getTranslations: function () {
+		return {
+			en: "translations/en.json",
+			sv: "translations/sv.json"
+		}
+	},
+
+	// Notification from node_helper.js.
+	// The stats is received here. Then module is redrawn.
+	// @param notification - Notification type.
+	// @param payload - Contains an array of user stats. Each item in the array contains userName / score / matchesPlayed / kills.
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'STATS_RESULT') {
+			if (null == payload)
+				return;
+
+			if (0 === payload.length)
+				return;
+
+			this.stats = payload;
+			this.updateDom(0);
+		}
+	},
+
+	// Override dom generator.
+	getDom: function () {
+		let wrapper = document.createElement('table');
+		if (null == this.stats) {
+			wrapper.innerHTML = this.translate('LOADING');
+			wrapper.className = 'dimmed xsmall';
+			return wrapper;
+		}
+		
+		wrapper.className = 'bright xsmall';
+
+		let headerRow = document.createElement('tr');
+		headerRow.className = 'normal headerrow';
+		this.createTableCell(headerRow, this.translate('USER_NAME'), true, true);
+		this.createTableCell(headerRow, this.translate('SCORE'), this.config.showScore);
+		this.createTableCell(headerRow, this.translate('MATCHES_PLAYED'), this.config.showMatchesPlayed);
+		this.createTableCell(headerRow, this.translate('KILLS'), this.config.showKills);
+		wrapper.appendChild(headerRow);
+
+		for (let i = 0; i < this.stats.length; ++i) {
+			let row = document.createElement('tr');
+			row.className = 'normal bright statsrow';
+
+			const stat = this.stats[i];
+			this.createTableCell(row, stat.userName, true, true);
+			this.createTableCell(row, stat.score, this.config.showScore);
+			this.createTableCell(row, stat.matchesPlayed, this.config.showMatchesPlayed);
+			this.createTableCell(row, stat.kills, this.config.showKills);
+
+			wrapper.appendChild(row);
+		}
+
+		return wrapper;
+	},
+
+	// Override start to init stuff.
+	start: function() {
+		this.stats = null;
+
+		// Tell node_helper to load stats at startup.
+		this.sendSocketNotification('GET_STATS', this.config.userIDs);
+
+		// Make sure stats are reloaded at user specified interval.
+		let interval = Math.max(this.config.fetchInterval, 1000);  // In millisecs. < 1 min not allowed.
+		let self = this;
+		setInterval(function() {
+			console.log('fortnite GET_STATS');
+			self.sendSocketNotification('GET_STATS', self.config.userIDs);
+		}, interval); // In millisecs.
+	},
+
+	// Creates a table row cell.
+	// @param row - The table row to add cell to.
+	// @param string - The text to show.
+	// @param show - Whether to actually show.
+	// @param leftAlign - True to left align text. False to center align.
+	createTableCell: function(row, string, show, leftAlign = false)
+	{
+		if (!show)
+			return;
+		
+		let cell = document.createElement('td');
+		cell.innerHTML = string;
+		
+		if (leftAlign)
+			cell.style.cssText = 'text-align: left;';
+		else
+			cell.style.cssText = 'text-align: right;';
+
+		row.appendChild(cell);
+	}
+});
---
+++ node_helper.js
@@ -0,0 +1,99 @@
+/* Magic Mirror
+ * Module: MMM-Fortnite
+ *
+ * By Johan Persson, https://github.com/retroflex
+ * MIT Licensed.
+ */
+
+const NodeHelper = require('node_helper');
+const rp = require('request-promise');
+const baseURL = 'https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=';
+
+module.exports = NodeHelper.create({
+	start: function() {
+		//console.log('Starting node_helper for: ' + this.name);
+	},
+
+	// Extracts JSON into the relevant stats.
+	// JSON can contain these (but not all are necessarily present):
+	//   json.overallData.defaultModes.score / .matchesPlayed / .kills
+	//   json.overallData.ltmModes.score / .matchesPlayed / .kills
+	//   json.overallData.largeTeamModes.score / .matchesPlayed / .kills
+	// @param json - The full JSON for the user.
+	// @return Object with stats we want to show.
+	extractStats: function(json) {
+		let totalScore = 0;
+		let totalMatchesPlayed = 0;
+		let totalKills = 0;
+
+		const defaultModes = json.overallData.defaultModes;
+		if (defaultModes != null) {
+			if (defaultModes.score         != null)  totalScore         += defaultModes.score;
+			if (defaultModes.matchesplayed != null)  totalMatchesPlayed += defaultModes.matchesplayed;
+			if (defaultModes.kills         != null)  totalKills         += defaultModes.kills;
+		}
+
+		const ltmModes = json.overallData.ltmModes;
+		if (ltmModes != null) {
+			if (ltmModes.score         != null)  totalScore         += ltmModes.score;
+			if (ltmModes.matchesplayed != null)  totalMatchesPlayed += ltmModes.matchesplayed;
+			if (ltmModes.kills         != null)  totalKills         += ltmModes.kills;
+		}
+
+		const largeTeamModes = json.overallData.largeTeamModes;
+		if (largeTeamModes != null) {
+			if (largeTeamModes.score         != null)  totalScore         += largeTeamModes.score;
+			if (largeTeamModes.matchesplayed != null)  totalMatchesPlayed += largeTeamModes.matchesplayed;
+			if (largeTeamModes.kills         != null)  totalKills         += largeTeamModes.kills;
+		}
+
+		const stats = { userName: json.epicName,
+		                score: totalScore,
+		                matchesPlayed: totalMatchesPlayed,
+		                kills: totalKills };
+		return stats;
+	},
+
+	// Gets Fortnite user stats from API and adds them to an array.
+	// Each item in the array contains userName / score / matchesPlayed / kills.
+	// The stats are the total for all seasons and all game modes.
+	// The array is then sent to the client (to MMM-Fortnite.js).
+	// @param userIDs - String array of user ID's.
+	getStats: function(userIDs) {
+		let promises = [];
+		for (let i = 0; i < userIDs.length; ++i)
+		{
+			const userURL = baseURL + userIDs[i];
+			const options = {uri: userURL};
+			promises.push(rp(options));
+		}
+
+		Promise.all(promises).then((contents) => {
+			let stats = [];
+
+			for (let i = 0; i < contents.length; ++i) {
+				const content = contents[i];
+				const json = JSON.parse(content);
+				
+				const stat = this.extractStats(json);
+				stats.push(stat);
+			}
+
+			stats.sort((a, b) => Number(b.score) - Number(a.score));  // Sort users by score.
+
+			this.sendSocketNotification('STATS_RESULT', stats);
+		}).catch(err => {
+			console.log(this.name + ' error when fetching data: ' + err);
+		});
+	},
+
+	// Listens to notifications from client (from MMM-Fortnite.js).
+	// Client sends a notification when it wants download new stats.
+	// @param payload - String array of user ID's.
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'GET_STATS') {
+			this.getStats(payload);
+		}
+	}
+
+});
